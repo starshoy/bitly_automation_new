410d11eab3cc713ae62a610e7baaa8124ea66d67
starshoy <starshoy1991@gmail.com>
2023-03-04 16:09:36 +0000

    Replace project from local storage to git repo

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bitly URL shortener
+
+Project is aimed to give user an ability to:
+- make shorten links via Bitly Url service
+- decode shorten Bitly links
+
+### How to install
+
+To be able to run the project `BITLY_OAUTH_TOKEN` should be stored in .env file.
+Register first on Bitly Service, to generate the token on https://app.bitly.com/settings/integrations/
+
+Python3 should already be installed. 
+Use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+This code was written for educational purposes as part of an online course for web developers at [dvmn.org](https://dvmn.org/).
---
+++ main.py
@@ -0,0 +1,72 @@
+import requests
+import validators
+import os
+import argparse
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
+def shorten_link(link, token):
+    host = "https://api-ssl.bitly.com/v4"
+    url = f"{host}/bitlinks"
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {"long_url": link}
+  
+    response = requests.post(url=url, 
+                             headers=headers, 
+                             json=payload)
+    response.raise_for_status() 
+
+    return response.json()["link"]
+
+
+def get_url_without_scheme(url):
+    parsed_url = urlparse(url)
+    return f"{parsed_url.netloc}{parsed_url.path}"
+
+
+def is_bitlink(link, token):
+    host = "https://api-ssl.bitly.com/v4"
+    url = f"{host}/bitlinks/{get_url_without_scheme(link)}"
+    headers = {"Authorization": f"Bearer {token}"}
+
+    response = requests.get(url=url, headers=headers)
+    return response.ok
+
+
+def input_url(): 
+    url = input("Введите ссылку: ")
+    if not validators.url(url):
+      raise TypeError("Invalid url. Please check it was type in a proper way")
+    return url
+  
+
+def count_clicks(link, token):
+    host = "https://api-ssl.bitly.com/v4"
+    url = f"{host}/bitlinks/{get_url_without_scheme(link)}/clicks/summary"
+    headers = {"Authorization": f"Bearer {token}"}
+  
+    response = requests.get(url=url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()["total_clicks"]
+
+
+def main():
+    load_dotenv()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="link to convert")
+    args = parser.parse_args()
+
+    bitly_token = os.environ["BITLY_OAUTH_TOKEN"]
+  
+    link = args.url
+    
+    if is_bitlink(link=link, token=bitly_token):
+        print(f"По вашей ссылке прошли: {count_clicks(link=link, token=bitly_token)} раз(а)")
+    else:
+        print(f"Битлинк: {shorten_link(link=link, token=bitly_token)}")
+
+if __name__ == "__main__":
+    main()
